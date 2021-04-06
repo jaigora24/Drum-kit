74ff2e971f96869d2fbdb807cf558fb4b647f6e5
Jai Gora <Jaigora24@gmail.com>
2021-04-07 02:12:53 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var clap = new Audio("Sounds/sounds_clap.ogg");
+var hihat = new Audio("Sounds/sounds_hihat.ogg");
+var kick = new Audio("Sounds/sounds_kick.ogg");
+var openhat = new Audio("Sounds/sounds_openhat.ogg");
+var boom = new Audio("Sounds/sounds_boom.ogg");
+var ride = new Audio("Sounds/sounds_ride.ogg");
+var snare = new Audio("Sounds/sounds_snare.ogg");
+var tom = new Audio("Sounds/sounds_tom.ogg");
+var tink = new Audio("Sounds/sounds_tink.ogg");
